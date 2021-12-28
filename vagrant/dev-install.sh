1bf6fee469b7b1989c5a217dbec097fd880c5521
Donovan C. Young <dyoung522@gmail.com>
2021-12-28 01:20:42 -0500

    Add zshrc

--- vagrant/dev-install.sh
+++ vagrant/dev-install.sh
@@ -3,3 +3,9 @@
 set -e
 
 echo "Running Donovan's Vagrant development environment configuration tool..."
+
+echo "Copying .gitconfig"
+curl -fsSL https://raw.githubusercontent.com/dyoung522/cookbooks/main/git/config -o ~/.gitconfig
+
+echo "Copying .zshrc"
+curl -fsSL https://raw.githubusercontent.com/dyoung522/cookbooks/main/zsh/config -o ~/.zshrc
