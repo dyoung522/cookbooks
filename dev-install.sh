80f7cd7d8e2eaf0bb5c86ce9222fd668c13243c5
Donovan Young <dyoung522@gmail.com>
2021-12-28 14:09:12 -0500

    Add rbenv configuration

--- dev-install.sh
+++ dev-install.sh
@@ -4,13 +4,21 @@ set -e
 
 echo "Running Donovan's Development Environment Configuration Tool..."
 
-if [ ! -f ~/.gitconfig ]; then
+RBENV_ROOT=$(rbenv root)
+if [[ -n "${RBENV_ROOT}" ]]; then
+  echo "Installing ruby-build"
+  [[ ! -d ${RBENV_ROOT}/plugins ]] && mkdir -p ${RBENV_ROOT}/plugins
+  [[ ! -d ${RBENV_ROOT}/plugins/ruby-build ]] && git clone https://github.com/rbenv/ruby-build.git ${RBENV_ROOT}/plugins/ruby-build
+  git -C ${RBENV_ROOT}/plugins/ruby-build pull
+fi
+
+if [[ ! -f ~/.gitconfig ]]; then
   echo "Installing new .gitconfig"
   curl -fsSL https://raw.githubusercontent.com/dyoung522/cookbooks/main/git/config -o ~/.gitconfig
 fi
 
-if [ -d ~/.oh-my-zsh ]; then
-  if [ -f ~/.zshrc ]; then
+if [[ -d ~/.oh-my-zsh ]]; then
+  if [[ -f ~/.zshrc ]]; then
     echo "Backing up existing .zshrc file"
     mv ~/.zshrc ~/.zshrc.backup
   fi
